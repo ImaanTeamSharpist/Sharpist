0a8d822bb1cfdda553a35bd15b39576d5b69a0c7
Dilafruza <dilafruzjraboyeva@gmail.com>
2024-07-23 09:52:19 +0500

    interfaces are done

--- Sharpist.Server.Service/IServices/IHRService.cs
+++ Sharpist.Server.Service/IServices/IHRService.cs
@@ -1,5 +1,15 @@
-﻿namespace Sharpist.Server.Service.IServices;
+﻿using Sharpist.Server.Domain.Configurations;
+using Sharpist.Server.Service.DTOs.HRs;
 
-public class IHRService
+namespace Sharpist.Server.Service.IServices;
+
+public interface IHRService 
 {
+    Task<bool> RemoveAsync(int id);
+    Task<HRForResultDto> RetrieveByIdAsync(int id);
+    Task<HRForResultDto> AddAsync(HRForCreationDto dto);
+    Task<HRForResultDto> ModifyAsync(int id, HRForUpdateDto dto);
+    Task<HRForResultDto> RetrieveByEmailAsync(string email);
+    Task<IEnumerable<HRForResultDto>> RetrieveAllAsync(PaginationParams @params);
+    Task<bool> ForgetPasswordAsync(string Email, string NewPassword, string ConfirmPassword);
 }
