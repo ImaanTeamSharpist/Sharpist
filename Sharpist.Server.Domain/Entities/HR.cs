927b8cb612b57a99ae76ac497b87db70ed8a8a3b
Dilafruza <dilafruzjraboyeva@gmail.com>
2024-07-23 08:00:13 +0500

    Add project files.

---
+++ Sharpist.Server.Domain/Entities/HR.cs
@@ -0,0 +1,10 @@
+﻿using Sharpist.Server.Domain.Entities.Commons;
+
+namespace Sharpist.Server.Domain.Entities;
+
+public class HR : Auditable
+{
+    public string Name { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Password { get; set; } = "";
+}
